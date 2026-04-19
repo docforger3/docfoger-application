65f3009b82e18e1e496387adf3a86f4c66773af2
Ankit23Khapre <ankit23khapre@gmail.com>
2026-04-19 11:59:11 +0530

    add clientId in prod ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   enableWordExport: true,
   adsensePublisherId: 'ca-pub-XXXXXXXXXXXXXXXX',
-  apiUrl: 'https://docforge-api.onrender.com' // Placeholder: Update with your Render URL
+  apiUrl: 'https://docforge-api.onrender.com', // Placeholder: Update with your Render URL
+  googleClientId: '343676903730-vn1btu2vgrsgtfdh407k229334r46mhg.apps.googleusercontent.com'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
   adsensePublisherId: 'ca-pub-XXXXXXXXXXXXXXXX',
   // Replace with YOUR Google OAuth Client ID from Google Cloud Console
   // https://console.cloud.google.com/ → APIs & Services → Credentials
-  googleClientId: '343676903730-vn1btu2vgrsgtfdh407k229334r46mhg.apps.googleusercontent.com'
+  googleClientId: '343676903730-vn1btu2vgrsgtfdh407k229334r46mhg.apps.googleusercontent.com',
+  apiUrl: 'http://localhost:3000'
 };
